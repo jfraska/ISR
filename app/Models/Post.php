07f73cd6e4c0f75150575974b9e66f23972ff611
Jfraska <davinfraska20@gmail.com>
2024-03-04 13:28:17 +0700

    feat: flowbite

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Str;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
 
@@ -26,5 +27,9 @@ class Post extends Model implements HasMedia
 
     public function user(): BelongsTo {
         return $this->belongsTo(User::class);
+    }
+
+    public function excerpt(): string {
+        return Str::words(strip_tags($this->content), 200, '...');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    $posts = Post::all();
+    return view('home', ['posts' => $posts]);
 });
