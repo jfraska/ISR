c51bfdf0da14502703fd4265206cc772038d617a
Alifian-Zulfaani <inaafilakoko27@gmail.com>
2024-03-12 10:58:50 +0700

    landing layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,9 +5,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>@yield('title', config('app.name', 'Laravel'))</title>
     <meta name="description" content="@yield('meta_description', 'Site description')" />
+
+    <title>{{ config('app.name') }} - @yield('title')</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
@@ -17,6 +17,7 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     <!-- Styles -->
+    <link rel="shortcut icon" href="/images/isr-logo.png">
     @livewireStyles
 </head>
 
@@ -24,28 +25,19 @@
     <x-banner />
 
     <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
+        <!-- Navbar & Header -->
         @include('layouts.partials.header')
-
-        {{-- <!-- Page Heading -->
-        @if (isset($header))
-            <header class="bg-white dark:bg-gray-800 shadow">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    {{ $header }}
-                </div>
-            </header>
-        @endif --}}
 
         <!-- Page Content -->
         <main>
-            {{ $slot }}
+            @yield('content')
         </main>
 
+        <!-- Footer -->
         @include('layouts.partials.footer')
     </div>
 
-
     @stack('modals')
-
     @livewireScripts
 </body>
 
--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,11 +1,20 @@
 <footer class="flex flex-wrap items-center justify-between px-4 py-4 text-sm border-t border-gray-100 ">
-
-    <div class="flex space-x-4">
-        {{-- @foreach (config('app.supported_locales') as $locale => $data)
-            <a href="{{ route('locale', $locale) }}">
-                <x-dynamic-component :component="'flag-country-' . $data['icon']" class="w-6 h-6" />
-            </a>
-        @endforeach --}}
-    </div>
-
+    {{-- <div class="">
+        <div class="grid grid-cols-4 gap-4">
+            <!-- Baris 1 -->
+            <div class="col-span-2 bg-gray-300 p-4">Kolom 1</div>
+            <div class="col-span-2 bg-gray-400 p-4">Kolom 2</div>
+          
+            <!-- Baris 2 -->
+            <div class="col-span-4 bg-gray-500 p-4">Kolom 3</div>
+          
+            <!-- Baris 3 -->
+            <div class="col-span-3 bg-gray-600 p-4">Kolom 4</div>
+            <div class="col-span-1 bg-gray-700 p-4">Kolom 5</div>
+          
+            <!-- Baris 4 -->
+            <div class="col-span-2 bg-gray-800 p-4">Kolom 6</div>
+            <div class="col-span-2 bg-gray-900 p-4">Kolom 7</div>
+          </div>
+    </div> --}}
 </footer>
