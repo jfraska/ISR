3676bc9e2a30bd70b13aeb41a8066c39f1c24e39
Jfraska <davinfraska20@gmail.com>
2024-03-13 09:50:04 +0700

    merge: frontend

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,13 +1,47 @@
-<footer
-    class="flex flex-wrap items-center justify-between border-t border-gray-100 px-4 py-4 text-sm"
->
-    <div class="flex space-x-4">
-        {{--
-            @foreach (config('app.supported_locales') as $locale => $data)
-            <a href="{{ route('locale', $locale) }}">
-            <x-dynamic-component :component="'flag-country-' . $data['icon']" class="w-6 h-6" />
-            </a>
-            @endforeach
-        --}}
+<footer class="bg-blue-500 w-full h-[400px] bottom-0 mt-20 flex justify-center">
+    <div class="w-3/4 pt-10">
+        <div class="flex w-full h-[250px]">
+            <div class="w-full pl-7 pr-3 border-r-2 border-white">
+                <div class="p-0">
+                    <img src="/images/isr-footer.png" alt="isr footer">
+                </div>
+            </div>
+            <div class="w-full pl-7 pr-3 border-r-2 border-white">
+                <div class="p-0 w-[400px]"
+                    style="position: relative; display: flex; align-items: flex-start; flex-direction: column">
+                    <div class="" style="flex-grow: 1;">
+                        <h1 style="font-weight: bold; font-size: 17px; color:white">Alamat</h1>
+                        <p style="margin-bottom: 0; font-size: 14px; color:white">Universitas Pembangunan Nasional
+                            "VETERAN" Yogyakarta <br> Jl. SWK No.104 Yogyakarta 55283 (Kampus Pusat)</p>
+                    </div>
+                    <div class="mt-10">
+                        <div class="flex flex-col">
+                            <div class="flex flex-row pb-3">
+                                <img class="w-5 h-5 mr-3" src="/images/telp.svg" alt="">
+                                <p style="font-weight: bold; font-size: 17px; color:white">Telp. +62 274 486733</p>
+                            </div>
+                            <div class="flex flex-row pb-3">
+                                <img class="w-5 h-5 mr-3 underline" src="/images/mail.svg" alt="">
+                                <a href="#"
+                                    style="font-weight: bold; font-size: 17px; color:white">info@upnyk.ac.id</a>
+                            </div>
+                            <div class="flex flex-row pb-3">
+                                <img class="w-5 h-5 mr-3" src="/images/map.svg" alt="">
+                                <a href="#" style="font-weight: bold; font-size: 17px; color:white">Lihat Peta</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="w-full pl-7 pr-3">
+                <div class="p-0"
+                    style="position: relative; display: flex; align-items: flex-start; flex-direction: column">
+                    <div class="" style="flex-grow: 1;">
+                        <h1 style="font-size: 14px; color:white">Mitra</h1>
+                        <h1 style="font-size: 14px; color:white">Kebijakan Pengguna</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </footer>
