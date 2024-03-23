09a9b04474d9d15e131548ce32574ec161f15181
Jfraska <davinfraska20@gmail.com>
2024-03-23 14:42:53 +0700

    fix: markdown

--- config/app.php
+++ config/app.php
@@ -171,7 +171,6 @@ return [
         App\Providers\RouteServiceProvider::class,
         App\Providers\FortifyServiceProvider::class,
         App\Providers\JetstreamServiceProvider::class,
-        App\Providers\MarkdownServiceProvider::class,
     ])->toArray(),
 
     /*
