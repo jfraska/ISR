e026daa2da8038a09a18b061dc0c08232ab69c63
Jfraska <davinfraska20@gmail.com>
2024-04-02 15:01:46 +0700

    merge: branch develop

--- app/Http/Controllers/OrganizationalController.php
+++ app/Http/Controllers/OrganizationalController.php
@@ -5,13 +5,13 @@ namespace App\Http\Controllers;
 use App\Models\Organizational;
 use Illuminate\Http\Request;
 
-class OrganizationController extends Controller
+class OrganizationalController extends Controller
 {
     public function visimisi()
     {
         $organizational = Organizational::all();
 
-        return view('abouts/visi-misi', [
+        return view('abouts.visi-misi', [
             'organizational' => $organizational,
         ]);
     }
