ee7c82e149c8af220d593eaeaa30c732896aeaa7
Alifian-Zulfaani <inaafilakoko27@gmail.com>
2024-04-30 15:16:22 +0700

    feat: sidenav component & fix: tentang isr responsive

--- resources/views/abouts/show.blade.php
+++ resources/views/abouts/show.blade.php
@@ -3,18 +3,18 @@
 @section('title', "$organizational->title")
 
 @section('content')
-    <div class="relative px-40 py-28 min-w-[240px]">
-        <div class="flex flex-row gap-10">
-            <div class="flex flex-col w-[550px] h-[450px] bg-[#0D5568]">
-
+    <div class="relative px-14 md:px-40 py-28 w-full">
+        <div class="flex flex-row gap-20 items-center lg:items-start justify-center">
+            <div class="lg:w-1/3 py-3 flex-col hidden lg:flex">
+                <livewire:side-navigation>
             </div>
-            <div class="flex flex-col min-w-[750px] max-w-[750px]">
+            <div class="flex flex-col w-full lg:w-2/3">
                 <h1 class="font-bold text-[32px]">{{ $organizational->title }}</h1>
-                <img src="{{ $organizational->getFirstMediaUrl() }}" alt="isr-logo" class="w-[350px] h-[350px] mx-auto py-5">
+                <img src="{{ $organizational->getFirstMediaUrl() }}" alt="" class="w-[350px] h-[350px] mx-auto py-5">
                 <x-content :items="$organizational->content"/>
             </div>
         </div>
-        <div class="absolute bottom-0 left-0">
+        <div class="absolute bottom-0 left-0 hidden lg:block">
             <img src="/images/side-decor-bottom-left.png" alt="" />
         </div>
     </div>
--- resources/views/components/content.blade.php
+++ resources/views/components/content.blade.php
@@ -2,25 +2,43 @@
 
 <div class="">
     @foreach ($items as $item)
+
         @if ($item['type'] === 'heading')
             @if ($item['data']['level'] === 'h1')
-                <h1>{!! $item['data']['content'] !!}</h1>
+                <h1 class="text-2xl font-bold mb-2">{!! $item['data']['content'] !!}</h1>
             @endif
             @if ($item['data']['level'] === 'h2')
-                <h2>{!! $item['data']['content'] !!}</h2>
+                <h2 class="text-xl font-bold mb-2">{!! $item['data']['content'] !!}</h2>
             @endif
             @if ($item['data']['level'] === 'h3')
-                <h3>{!! $item['data']['content'] !!}</h3>
+                <h3 class="text-lg font-bold mb-2">{!! $item['data']['content'] !!}</h3>
             @endif
         @endif
+
         @if ($item['type'] === 'paragraph')
-            <p class="text-sm font-bold">{!! $item['data']['content'] !!}</p>
+            @php
+                $content = $item['data']['content'];
+                // Tambahkan kelas jika tag <p> ditemukan
+                $modifiedContent = preg_replace(
+                    '/<p>/',
+                    '<p class="text-md font-regular text-justify mb-5">',
+                    $content,
+                );
+                // Tambahkan kelas jika tag <ol> ditemukan
+                $modifiedContent = preg_replace('/<ol>/', '<ol class="text-md font-regular text-justify mb-5">', $modifiedContent);
+                // Tambahkan kelas jika tag <li> ditemukan
+                $modifiedContent = preg_replace('/<li>/', '<li class="list-decimal text-md font-regular text-justify ml-5 mb-2">', $modifiedContent);
+            @endphp
+            {!! $modifiedContent !!}
         @endif
+
         @if ($item['type'] === 'image')
             <img src="{{ $item['data']['url'] }}" alt="{{ $item['data']['alt'] }}">
         @endif
+
         {{-- @if ($item['type'] === 'attachment')
             <img src="{{ $item['data']['url'] }}" alt="{{ $item['data']['alt'] }}">
         @endif --}}
+
     @endforeach
 </div>
