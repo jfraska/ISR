442bf42ac8f005624c47252c5154a44463ec850d
Jfraska <davinfraska20@gmail.com>
2024-05-06 09:55:09 +0700

    fix: content and card-list

--- resources/views/components/content.blade.php
+++ resources/views/components/content.blade.php
@@ -1,48 +1,55 @@
-@props([
-    "items",
-])
+@props(['items'])
 
 <div class="">
     @foreach ($items as $item)
-        @if ($item["type"] === "heading")
-            @if ($item["data"]["level"] === "h1")
+        @if ($item['type'] === 'heading')
+            @if ($item['data']['level'] === 'h1')
                 <h1 class="mb-2 py-5 text-2xl font-bold">
-                    {!! $item["data"]["content"] !!}
+                    {!! $item['data']['content'] !!}
                 </h1>
             @endif
 
-            @if ($item["data"]["level"] === "h2")
+            @if ($item['data']['level'] === 'h2')
                 <h2 class="mb-2 py-3 text-xl font-bold">
-                    {!! $item["data"]["content"] !!}
+                    {!! $item['data']['content'] !!}
                 </h2>
             @endif
 
-            @if ($item["data"]["level"] === "h3")
+            @if ($item['data']['level'] === 'h3')
                 <h3 class="mb-2 py-2 text-lg font-bold">
-                    {!! $item["data"]["content"] !!}
+                    {!! $item['data']['content'] !!}
                 </h3>
             @endif
         @endif
 
-        @if ($item["type"] === "paragraph")
+        @if ($item['type'] === 'paragraph')
             @php
-                $content = $item["data"]["content"];
+                $content = $item['data']['content'];
                 // Tambahkan kelas jika tag <p> ditemukan
-                $modifiedContent = preg_replace("/<p>/", '<p class="text-md font-regular text-justify mb-5">', $content);
+                $modifiedContent = preg_replace(
+                    '/<p>/',
+                    '<p class="text-md font-regular text-justify mb-5">',
+                    $content,
+                );
                 // Tambahkan kelas jika tag <ol> ditemukan
-                $modifiedContent = preg_replace("/<ol>/", '<ol class="text-md font-regular text-justify mb-5">', $modifiedContent);
+                $modifiedContent = preg_replace(
+                    '/<ol>/',
+                    '<ol class="text-md font-regular text-justify mb-5">',
+                    $modifiedContent,
+                );
                 // Tambahkan kelas jika tag <li> ditemukan
-                $modifiedContent = preg_replace("/<li>/", '<li class="list-decimal text-md font-regular text-justify ml-5 mb-2">', $modifiedContent);
+                $modifiedContent = preg_replace(
+                    '/<li>/',
+                    '<li class="list-decimal text-md font-regular text-justify ml-5 mb-2">',
+                    $modifiedContent,
+                );
             @endphp
 
             {!! $modifiedContent !!}
         @endif
 
-        @if ($item["type"] === "image")
-            <img
-                src="{{ $item["data"]["url"] }}"
-                alt="{{ $item["data"]["alt"] }}"
-            />
+        @if ($item['type'] === 'image')
+            <img src="/{{ $item['data']['url'] }}" alt="{{ $item['data']['alt'] }}" />
         @endif
 
         {{--
--- resources/views/components/posts/post-item.blade.php
+++ resources/views/components/posts/post-item.blade.php
@@ -1,19 +1,10 @@
-@props([
-    "post",
-])
+@props(['post'])
 
 <article class="border-gray-100 pb-10 [&:not(:last-child)]:border-b">
     <div class="article-body mt-5 grid grid-cols-12 items-start gap-3">
         <div class="col-span-4">
-            <a
-                href="{{ route("posts.show", ["category" => $post->categories->slug, "post" => $post->slug]) }}"
-            >
-                <img
-                    class="h-full w-full rounded"
-                    src="B5kAk8u5ju0PKQtfv5nAh3KYX2oFWIyB"
-                    ="B5kAk8u5ju0PKQtfv5nAh3KYX2oFWIyB"
-                    alt="{{ $post->title }}"
-                />
+            <a href="{{ route('posts.show', ['category' => $post->categories->slug, 'post' => $post->slug]) }}">
+                <img class="h-full w-full rounded" src="{{ $post->getFirstMediaUrl() }}" alt="{{ $post->title }}" />
             </a>
         </div>
         <div class="col-span-8 pl-3">
@@ -24,9 +15,7 @@
                 </span>
             </div>
             <h2 class="text-md font-bold text-gray-900">
-                <a
-                    href="{{ route("posts.show", ["category" => $post->categories->slug, "post" => $post->slug]) }}"
-                >
+                <a href="{{ route('posts.show', ['category' => $post->categories->slug, 'post' => $post->slug]) }}">
                     {{ $post->title }}
                 </a>
             </h2>
@@ -37,15 +26,10 @@
             <p class="mt-2 text-sm font-light text-gray-700">
                 {{ $strippedExcerpt }}
             </p>
-            <div
-                class="article-actions-bar mt-6 flex items-center justify-between"
-            >
+            <div class="article-actions-bar mt-6 flex items-center justify-between">
                 <div class="flex gap-x-2">
                     @foreach ($post->tags as $tag)
-                        <x-badge
-                            wire:navigate
-                            href="{{ route('posts.index', ['tag' => $tag->slug]) }}"
-                        >
+                        <x-badge wire:navigate href="{{ route('posts.index', ['tag' => $tag->slug]) }}">
                             {{ $tag->name }}
                         </x-badge>
                     @endforeach
@@ -62,10 +46,7 @@
                         <img src="/images/view.png" alt="view" class="w-5" />
                         {{ $post->getPageViews() }}
                     </span>
-                    <livewire:like-button
-                        :key="'like-' . $post->id"
-                        :model="$post"
-                    />
+                    <livewire:like-button :key="'like-' . $post->id" :model="$post" />
                 </div>
             </div>
         </div>
